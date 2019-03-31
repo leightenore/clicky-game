a44e149ef72f41a655270620de015d5226e10106
ltenore <leightenore@gmail.com>
2019-03-31 12:21:29 -0700

    Updates

---
+++ precache-manifest.086722fa32a3246d0aef7aa0887c8cb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cf4ecc739064a8ab73c37c2f980d7b5",
+    "url": "/clicky-game/static/media/background.1cf4ecc7.jpg"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.895984e4.js"
+  },
+  {
+    "revision": "f9f7896559a058bb3b7d",
+    "url": "/clicky-game/static/js/main.ca43e337.chunk.js"
+  },
+  {
+    "revision": "87b35ee587fdfb09cbff",
+    "url": "/clicky-game/static/js/2.d4fab23f.chunk.js"
+  },
+  {
+    "revision": "f9f7896559a058bb3b7d",
+    "url": "/clicky-game/static/css/main.2e6ed333.chunk.css"
+  },
+  {
+    "revision": "87b35ee587fdfb09cbff",
+    "url": "/clicky-game/static/css/2.acadc17e.chunk.css"
+  },
+  {
+    "revision": "2d79f80829e48f92264647e8b4e0cd79",
+    "url": "/clicky-game/index.html"
+  }
+];
